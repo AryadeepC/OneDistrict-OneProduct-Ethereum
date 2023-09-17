13ed1e39e25a66574e2a0ea77827145c5cb4a7b2
Arbiter_Aryad <notanarbiter@gmail.com>
2023-09-17 19:52:41 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 
 function App() {
-  const val = import.meta.env.ALCHEMY_HTTP_ENDPOINT;
-  console.log(val);
-
   return (
     <>
-      danza kuduro
+      Dear MC and SPN,
+      <br />
+      <br />
+      <Link to="/product">TIMELINE</Link>
+      <br />
+      <br />
+      <Link to="/add">LINK</Link>
     </>
   );
 }
